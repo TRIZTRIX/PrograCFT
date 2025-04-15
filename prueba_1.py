74d45123a6119b37c2c125e28747cd0b6e6cadfe
TRIZTRIX <e.freites@est.tecnologicovalparaiso.cl>
2025-04-15 11:37:20 -0400

    primera evaluación

---
+++ prueba_1.py
@@ -0,0 +1,45 @@
+#Dar la bienvenida al usuario
+print("Bienvenidos a la compañia telefónica ClaroStar")
+#Socilitar al cliente ingresar 
+dia= int (input("Por favor ingrese los minutos hablados durante el día: "))
+noche= int (input("Por favor ingrese los minutos hablados durante la noche: "))
+
+# Variables que se van a calcular
+costo_dia=0
+costo_noche=0
+exceso_dia=0
+exceso_noche=0
+
+#Calculos de los costos por minuto y excedentes por minuto
+if dia <= 100:
+    costo_dia = dia * 10
+
+else:
+    exceso_dia = dia - 100
+    costo_dia = (100 * 10) + (exceso_dia * 15)
+
+if noche <= 80:
+    costo_noche = noche * 7
+
+else:
+    exceso_noche = noche - 80
+    costo_noche = (80 * 7) + (exceso_noche * 13)
+
+#Cálculo del total
+total = costo_dia + costo_noche
+
+#Muestrar por pantalla los resultados
+print(f"Exceso en horario día: {exceso_dia} minutos")
+print(f"Exceso en horario noche: {exceso_noche} minutos")
+print(f"Total a pagar: {total} CLP") 
+
+#Mostrar por pantalla si el cliente excedió o no con el plan 
+if exceso_dia > 0 and exceso_noche > 0:
+    print("El cliente excedio los minutos del plan")
+elif exceso_dia > 0:
+    print("El cliente excedio los minutos del horario diurno")
+elif exceso_noche > 0: 
+    print("El cliente excedio los minutos del plan nocturno")
+else:
+    print("El cliente, no excedio los minutos del plan")
+
